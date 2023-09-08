7efc75f5a7aab8ae8fdae62227b3e36d3831837a
DJmovie12 <127426391+DJmovie12@users.noreply.github.com>
2023-09-08 19:25:53 +0300

    s

---
+++ script.js
@@ -0,0 +1,17 @@
+const img1= document.getElementById('site1');
+const img2= document.getElementById('site2');
+const img3= document.getElementById('site3');
+const img4= document.getElementById('site4');
+
+img1.addEventListener("click", function(){
+    window.location.href = "https://www.youtube.com/watch?v=QXkNeiNzDOU";
+})
+img2.addEventListener("click", function(){
+    window.location.href = "https://key-drop.com/?code=BURAKSASA";
+})
+img3.addEventListener("click", function(){
+    window.location.href = "https://hellcase.com/fburaksasa";
+})
+img4.addEventListener("click", function(){
+    window.location.href = "https://hellstore.me/r/buraksasa";
+})
