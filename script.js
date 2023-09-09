9a8dced75477afedad674e68c84999fc70837797
DJmovie12 <127426391+DJmovie12@users.noreply.github.com>
2023-09-09 20:59:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,33 @@ const img2= document.getElementById('site2');
 const img3= document.getElementById('site3');
 const img4= document.getElementById('site4');
 
-img1.addEventListener("click", function(){
+var clickcount = parseInt(localStorage.getItem('clickcount')) || 0;
+
+function incrementClickCount() {
+    clickcount++;
+    localStorage.setItem('clickcount', clickcount.toString());
+}
+
+img1.addEventListener("click", function () {
+    incrementClickCount();
+    console.log("Toplam tıklama sayısı: " + clickcount);
     window.location.href = "https://www.youtube.com/watch?v=QXkNeiNzDOU";
-})
-img2.addEventListener("click", function(){
+});
+
+img2.addEventListener("click", function () {
+    incrementClickCount();
+    console.log("Toplam tıklama sayısı: " + clickcount);
     window.location.href = "https://key-drop.com/?code=BURAKSASA";
-})
-img3.addEventListener("click", function(){
+});
+
+img3.addEventListener("click", function () {
+    incrementClickCount();
+    console.log("Toplam tıklama sayısı: " + clickcount);
     window.location.href = "https://hellcase.com/fburaksasa";
-})
-img4.addEventListener("click", function(){
+});
+
+img4.addEventListener("click", function () {
+    incrementClickCount();
+    console.log("Toplam tıklama sayısı: " + clickcount);
     window.location.href = "https://hellstore.me/r/buraksasa";
-})
+});
